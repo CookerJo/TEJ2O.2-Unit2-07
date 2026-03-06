39cd8f4934db74a03fcde1b95b9ffab230c6c45e
Josiah-Rigg <josiah.rigg@stu.ocsb.ca>
2026-03-05 21:43:45 -0500

    Made program

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Josiah
+ * Created on: March 2026
+ * This program will do cookie clicker
 */
 
-basic.showString('Hello, World!')
+// variables
+let cookies: number = 0
+
+// start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+input.onButtonPressed(Button.A, function () {
+
+    cookies = cookies + 1
+    basic.showNumber(cookies)
+}) 
+
+
+// stop
+input.onButtonPressed(Button.B, function () { 
+    cookies = 0
+    basic.showNumber(cookies)
+})
